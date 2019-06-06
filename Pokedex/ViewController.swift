18c00f9f8264f891b820f6e4b8389949f6f194a0
Jorge Castellon Jr <jorge.castellon.jr@gmail.com>
2019-06-05 20:34:19 -0700

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by jorge castellon jr. on 6/5/19.
+//  Copyright © 2019 Dranix Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
